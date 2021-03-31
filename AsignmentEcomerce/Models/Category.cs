810d511d8dbe8c5db63f16fa7586391f31a0812a
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-03-31 14:17:03 +0700

    Add Models:  class Category

---
+++ AsignmentEcomerce/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Product = new HashSet<Products>();
+        }
+        [Key]
+        public int IDCategory { get; set; }
+        [Required]
+        public string NameCategory { get; set; }
+        public ICollection<Products> Product { get; set; }
+    }
+}

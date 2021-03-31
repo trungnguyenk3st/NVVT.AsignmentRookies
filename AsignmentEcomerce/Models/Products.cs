8026444c2bf5acbfcdfc319714cdc201b0eb2c67
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-03-31 14:17:20 +0700

    Add Models:  class Products

---
+++ AsignmentEcomerce/Models/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Models
+{
+    public class Products
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IDProduct { get; set; }
+        [Required]
+        public string NameProduct { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public double UnitPrice { get; set; }
+        public string Image { get; set; }
+        public int IDCategory { get; set; }
+        [ForeignKey("IDCategory")]
+        public Category Category { get; set; }
+    }
+}

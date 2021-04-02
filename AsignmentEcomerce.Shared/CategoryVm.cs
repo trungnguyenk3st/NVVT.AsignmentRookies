0eb7f2442f03d51894d3bfa6443eea13efccdcee
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-02 13:33:29 +0700

    Update CategoryVm

---
+++ AsignmentEcomerce.Shared/CategoryVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Shared
+{
+    class CategoryVm
+    {
+        [Key]
+        public int IDCategory { get; set; }
+        [Required]
+        public string NameCategory { get; set; }
+    }
+}

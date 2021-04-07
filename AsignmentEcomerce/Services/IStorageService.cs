256068007c66a523eaf3eb745f33b7fa90e2aa2e
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-07 15:27:58 +0700

    Add file Service

---
+++ AsignmentEcomerce/Services/IStorageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Services
+{
+    public interface IStorageService
+    {
+        string GetFileUrl(string fileName);
+
+        Task SaveFileAsync(Stream mediaBinaryStream, string fileName);
+
+        Task DeleteFileAsync(string fileName);
+    }
+}

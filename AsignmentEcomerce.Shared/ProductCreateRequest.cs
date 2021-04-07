b4f9480c7f9bd025db07d8044cd5a7ec4990b3fa
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-07 15:28:34 +0700

    Update ShareVm

---
+++ AsignmentEcomerce.Shared/ProductCreateRequest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Shared
+{
+    public class ProductCreateRequest
+    {
+        public int IDProduct { get; set; }
+        public string NameProduct { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double UnitPrice { get; set; }
+        public string Description { get; set; }
+        public IFormFile ImageUrl { get; set; }
+
+        public int IDCategory { get; set; }
+    
+        
+
+    }
+}
---
+++ AsignmentEcomerce.Shared/ProductVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Shared
+{
+     public class ProductVm
+    {
+        public int IDProduct { get; set; }
+        public string NameProduct { get; set; }
+  
+        public int Quantity { get; set; }
+
+        public double UnitPrice { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

93de9898ebdacf8940dc20977b8e59781b54d593
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-08 15:27:14 +0700

    fix ProductApiClient

--- CustomerSite/Services/Product/IProductApiClient.cs
+++ CustomerSite/Services/Product/IProductApiClient.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CustomerSite.Services.Product
+namespace CustomerSite.Services
 {
     public interface IProductApiClient
     {
--- CustomerSite/Services/Product/ProductApiClient.cs
+++ CustomerSite/Services/Product/ProductApiClient.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CustomerSite.Services.Product
 {
@@ -18,44 +19,27 @@ namespace CustomerSite.Services.Product
 
         public async Task<IList<ProductVm>> GetProducts()
         {
-            var response = await _client.GetAsync("https://localhost:44317/api/product");
+            var response = await _client.GetAsync("https://localhost:44342/api/products");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<IList<ProductVm>>();
         }
 
         public async Task<ProductVm> GetProduct(int id)
         {
-            var response = await _client.GetAsync("https://localhost:44317/api/product/" + id);
+            var response = await _client.GetAsync("https://localhost:44342/api/products/" + id);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<ProductVm>();
         }
 
         public async Task<IList<ProductVm>> GetProductByCategory(int id)
         {
-            var response = await _client.GetAsync("https://localhost:44317/api/product/category/" + id);
+            var response = await _client.GetAsync("https://localhost:44342/api/products/category/" + id);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsAsync<IList<ProductVm>>();
         }
 
-        public async Task<IList<ProductVm>> GetProductByBrand(int id)
-        {
-            var response = await _client.GetAsync("https://localhost:44317/api/product/brand/" + id);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsAsync<IList<ProductVm>>();
-        }
+       
 
-        public async Task<IList<ProductVm>> GetProductByArray(List<int> temp)
-        {
-            List<ProductVm> lstProduct = new List<ProductVm>();
-            if (temp == null)
-                return lstProduct;
-            foreach (int id in temp)
-            {
-                var response = await _client.GetAsync("https://localhost:44317/api/product/" + id);
-                response.EnsureSuccessStatusCode();
-                lstProduct.Add(await response.Content.ReadAsAsync<ProductVm>());
-            }
-            return lstProduct;
-        }
+       
     }
 }

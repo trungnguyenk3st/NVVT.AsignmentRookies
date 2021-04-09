4485645a5bcfd1fa04264e25b316100ea70bfc2b
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-09 17:06:46 +0700

    Update Model RatingUpdate (public int TotalStar)

---
+++ AsignmentEcomerce/Migrations/20210409100532_UpdateRating.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsignmentEcomerce.Migrations
+{
+    public partial class UpdateRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "RatingProducts",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalStar",
+                table: "RatingProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RatingProducts_ApplicationUserId",
+                table: "RatingProducts",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RatingProducts_AspNetUsers_ApplicationUserId",
+                table: "RatingProducts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RatingProducts_AspNetUsers_ApplicationUserId",
+                table: "RatingProducts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RatingProducts_ApplicationUserId",
+                table: "RatingProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "RatingProducts");
+
+            migrationBuilder.DropColumn(
+                name: "TotalStar",
+                table: "RatingProducts");
+        }
+    }
+}

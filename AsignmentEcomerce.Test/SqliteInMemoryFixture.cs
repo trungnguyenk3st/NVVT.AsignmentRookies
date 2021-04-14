bd501e172aa53e6d70bec5efbe5401d80a403077
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-14 13:43:31 +0700

    Add Project test and class SQLiteMemoryFixture

---
+++ AsignmentEcomerce.Test/SqliteInMemoryFixture.cs
@@ -0,0 +1,60 @@
+﻿using AsignmentEcomerce.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Test
+{
+    public class SqliteInMemoryFixture: IDisposable
+    {
+        private IServiceScope _serviceScope;
+        private SqliteConnection _connection;
+
+        public virtual IServiceProvider ServiceProvider
+        {
+            get
+            {
+                if (_serviceScope == null)
+                {
+                    _serviceScope = ConfigureServices(new ServiceCollection())
+                        .BuildServiceProvider()
+                        .CreateScope();
+                }
+
+                return _serviceScope.ServiceProvider;
+            }
+        }
+
+        public virtual ApplicationDbContext Context
+            => ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        public virtual void CreateDatabase()
+        {
+            Dispose();
+            _connection = new SqliteConnection("Data Source=:memory:");
+            _connection.Open();
+            Context.Database.EnsureCreated();
+        }
+
+        public virtual void Dispose()
+        {
+            _connection?.Close();
+            _connection?.Dispose();
+            _serviceScope?.Dispose();
+            _serviceScope = null;
+        }
+
+        public virtual IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(b =>
+                b.UseSqlite(_connection));
+
+            return services;
+        }
+    }
+}

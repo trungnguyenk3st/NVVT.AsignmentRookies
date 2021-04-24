ff1bbfc17d38449d98c9f9e0b4311148529d86b3
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-24 15:53:18 +0700

    Add User API

---
+++ CustomerSite/Services/Users/IUserApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services.Users
+{
+    public interface IUserApiClient
+    {
+        Task<IList<UserVm>> GetUsers();
+    }
+}
---
+++ CustomerSite/Services/Users/UserApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSite.Services.Users
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly HttpClient _client;
+        public UserApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient("host");
+
+        }
+
+        public async Task<IList<UserVm>> GetUsers()
+        {
+            var response = await _client.GetAsync("api/user");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsAsync<IList<UserVm>>();
+        }
+    }
+}

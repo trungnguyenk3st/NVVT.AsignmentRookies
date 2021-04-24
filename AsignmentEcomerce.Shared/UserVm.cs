58c2899bf21663d7e20735e9c1bb5b4a87a23946
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-24 15:51:24 +0700

    Add User Controller and UserVm

---
+++ AsignmentEcomerce.Shared/UserVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Shared
+{
+    public class UserVm
+    {
+        public string id_user { get; set; }
+
+        public string mail_user { get; set; }
+
+        public string phone { get; set; }
+    }
+}
---
+++ AsignmentEcomerce/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using AsignmentEcomerce.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsignmentEcomerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<UserVm>>> GetUsers()
+        {
+            return await _context.Users
+                .Where(x => x.Email != "admin@gmail.com")
+                .Select(x => new UserVm
+                {
+                    id_user = x.Id,
+                    mail_user = x.Email,
+                    phone = x.PhoneNumber,
+
+                })
+                .ToListAsync();
+        }
+    }
+}

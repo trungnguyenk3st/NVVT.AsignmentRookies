47faf7c0c2645d9075c160b34705d59ca77af3d4
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-26 11:48:38 +0700

    import react-redux and authService

--- AsignmentEcomerce.React/AdminSite/asignment_react/src/App.js
+++ AsignmentEcomerce.React/AdminSite/asignment_react/src/App.js
@@ -3,12 +3,31 @@ import Header from "./components/Header";
 import Navigate from "./components/Navigate";
 import PageLayout from "./containers/PageLayout";
 import Routes from "./routes";
+import React from "react";
+//auth
+import { Provider } from "react-redux";
+import store from "./store";
+import userManager, { loadUserFromStorage } from "./services/authService";
+import AuthProvider from "./utils/authProvider.js";
 
 function App() {
+  React.useEffect(() => {
+    // fetch current user from cookies
+    loadUserFromStorage(store);
+  }, []);
+
   return (
-    <Router>
-      <PageLayout header={<Header />} nav={<Navigate />} content={<Routes />} />
-    </Router>
+    <Provider store={store}>
+      <AuthProvider userManager={userManager} store={store}>
+        <Router>
+          <PageLayout
+            header={<Header />}
+            nav={<Navigate />}
+            content={<Routes />}
+          />
+        </Router>
+      </AuthProvider>
+    </Provider>
   );
 }
 export default App;

c79487fb9bcff22aa6844d33659257ae09c61e8f
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-26 10:01:46 +0700

    import OIDC

--- AsignmentEcomerce.React/AdminSite/asignment_react/src/routes.js
+++ AsignmentEcomerce.React/AdminSite/asignment_react/src/routes.js
@@ -1,40 +1,31 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 //page comp
-import Dashboard from "./pages/dashboard/Dashboard";
+
 import Product from "./pages/products/Product";
 import Category from "./pages/categories/Category";
-import Order from "./pages/orders/Order";
 import User from "./pages/users/User";
 import NotMatch from "./pages/errors/NotMatch";
 import ProductDetail from "./pages/product-detail/ProductDetail";
+//authen
+import SigninOidc from "./pages/auth/Signin-oidc";
+import SignoutOidc from "./pages/auth/Signout-oidc";
+import PrivateRoute from "./utils/protectedRoute";
 
 
 export default function Routes(props) {
   return (
     <Switch>
-      {/* <Route exact path="/">
-        <Dashboard />
-      </Route>
-      <Route path="/orders">
-        <Order />
-      </Route> */}
-      <Route path="/products">
-        <Product />
-      </Route>
-      <Route path="/product/:id">
-        <ProductDetail/>
-      </Route>
-     
-      <Route path="/categories">
-        <Category />
-      </Route>
-      <Route path="/users">
-        <User />
-      </Route>
-      <Route path="*">
-        <NotMatch />
-      </Route>
+      <Route path="/signout-oidc" component={SignoutOidc} />
+      <Route path="/signin-oidc" component={SigninOidc} />
+
+      <Redirect exact from="/" to="/products" />
+
+      <PrivateRoute path="/products" component={Product} />
+      <PrivateRoute path="/categories" component={Category} />
+      <PrivateRoute path="/users" component={User} />
+
+      <Route path="*" component={NotMatch} />
     </Switch>
   );
 }

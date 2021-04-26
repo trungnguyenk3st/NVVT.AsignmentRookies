32c15af235df613f8111a48ab288342d1eafb51f
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-04-26 16:52:23 +0700

    fix UI ProductDetails

--- AsignmentEcomerce.React/AdminSite/asignment_react/src/pages/product-detail/ProductDetail.js
+++ AsignmentEcomerce.React/AdminSite/asignment_react/src/pages/product-detail/ProductDetail.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Button } from "reactstrap";
 import { useHistory, useParams } from "react-router";
 import SingleLayout from "../../containers/SingleLayout";
+// import SplitLayout from "../../containers/SingleLayout";
 import EditProduct from "./EditProduct";
+
 
 import _proSer from "../../services/productService";
 

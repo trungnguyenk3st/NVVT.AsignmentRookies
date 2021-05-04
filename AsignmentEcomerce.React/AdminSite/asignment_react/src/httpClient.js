a1026f03c354ef65c54d65dded7d99476404edc7
DESKTOP-QK5LU9M\Trung <trungnguyenk3st@gmail.com>
2021-05-04 15:02:34 +0700

    Change connectionStrings and ClientUrl for deploy Azure

--- AsignmentEcomerce.React/AdminSite/asignment_react/src/httpClient.js
+++ AsignmentEcomerce.React/AdminSite/asignment_react/src/httpClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://localhost:44342/api",
+  baseURL: "https://backende069e98663574dd58d6fe43c48b63058.azurewebsites.net/api",
 });
 
 export default instance;
--- AsignmentEcomerce.React/AdminSite/asignment_react/src/services/authService.js
+++ AsignmentEcomerce.React/AdminSite/asignment_react/src/services/authService.js
@@ -2,13 +2,13 @@ import { UserManager } from "oidc-client";
 import { storeUserError, storeUser } from "../actions/authAction";
 
 const config = {
-  authority: "https://localhost:44342",
+  authority: "https://backende069e98663574dd58d6fe43c48b63058.azurewebsites.net",
   client_id: "react_app",
-  redirect_uri: "http://localhost:3000/signin-oidc",
+  redirect_uri: "https://sad52fe2d276454672b1548a.z23.web.core.windows.net/signin-oidc",
   response_type: "id_token token",
   scope: "openid profile rookieshop.api",
-  post_logout_redirect_uri: "http://localhost:3000/signout-callback-oidc",
-  front_channel_logout_uri: "http://localhost:3000/signout-oidc",
+  post_logout_redirect_uri: "https://sad52fe2d276454672b1548a.z23.web.core.windows.net/signout-callback-oidc",
+  front_channel_logout_uri: "https://sad52fe2d276454672b1548a.z23.web.core.windows.net/signout-oidc",
 };
 
 const userManager = new UserManager(config);
